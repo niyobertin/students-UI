a3094ea7a7d47d4789f189a7428e3050a21ffca2
niyobertin <85437905+niyobertin@users.noreply.github.com>
2022-08-24 16:57:21 +0200

    form validation

--- js/register.js
+++ js/register.js
@@ -6,26 +6,56 @@ function addStudent() {
     let gender = document.getElementById("gender").value;
     let email = document.getElementById("email").value;
     let grade = document.getElementById("grade").value;
+    // form validation
 
-    const student = {
-        firstName: firstName,
-        lastName: lastName,
-        gender: gender,
-        email: email,
-        grade: grade,
+
+    let text1 = document.getElementById("text1");
+    let text2 = document.getElementById("text2");
+    let text3 = document.getElementById("text3");
+    let text4 = document.getElementById("text4");
+    let text5 = document.getElementById("text5");
+
+    if (firstName === "" || null || firstName.trim().length === 0) {
+        text1.style.color = "red";
+        text1.innerHTML = "First name is required";
+    } else if (lastName === "" || null || lastName.trim().length <= 2) {
+        text2.style.color = "red";
+        text2.innerHTML = "Last name box can not be empty";
+    } else if (null || gender.trim().length === 0) {
+        text3.style.color = "red";
+        text3.innerHTML = "Gender is required";
+    } else if (email === '' || null || email.trim().length === 0) {
+        text4.style.color = "red";
+        text4.innerHTML = "your email is required";
+
+    } else if (grade === '' || null || grade.trim().length === 0) {
+        text5.style.color = "red";
+        text5.innerHTML = "class is required";
+    } else {
+        const student = {
+            firstName: firstName,
+            lastName: lastName,
+            gender: gender,
+            email: email,
+            grade: grade,
+        }
+        fetch('http://localhost:8000/students', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(student)
+        })
+            .then(response => response.text())
+            .then(json => {
+                const result = document.getElementById("result");
+                result.innerHTML = `Student Added 🤗🤣😁`
+            })
+            .catch(err => console.log(err));
     }
-    fetch('http://localhost:8000/students', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-
-        body: JSON.stringify(student)
-    })
-        .then(response => response.text())
-        .then(json => console.log(json))
-        .catch(err => console.log(err));
 }
+
 
 const loopTroughStudents = (listOfStudents) => {
     for (let i = 0; i < listOfStudents.length; i++) {
@@ -68,11 +98,11 @@ const loopTroughStudents = (listOfStudents) => {
     }
 }
 function displayStudents() {
+
     fetch('http://localhost:8000/students')
         .then((response) => response.json())
         .then((data) => loopTroughStudents(data))
         .catch(err => console.log(err));
-
 }
 
 const removeStudent = () => {
@@ -85,7 +115,7 @@ const removeStudent = () => {
     })
         .then(res => {
             if (res) {
-                prompt("deleting data successfully")
+                res.send("deleting data successfully")
             } else {
                 console.log("deletindg data unsuccessfully")
             }
